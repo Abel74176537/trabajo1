00ca097bcef73a125ceb5e9e358a0096747ea326
abel592 <abeljosuecolquehuanca12@gmail.com>
2025-09-22 15:23:10 -0500

    abel543

--- ms-matricula/src/main/java/pe/edu/upeu/msmatricula/Entity/Matricula.java
+++ ms-matricula/src/main/java/pe/edu/upeu/msmatricula/Entity/Matricula.java
@@ -1,18 +1,21 @@
 package pe.edu.upeu.msmatricula.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "matricula")  // 👈 nombre exacto de la tabla
 public class Matricula {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_matricula")   // 👈 nombre real de la columna en la BD
     private Integer idMatricula;
+
+    @Column(name = "nombre_alumno")  // opcional, si quieres respetar snake_case en BD
     private String nombreAlumno;
+
+    @Column(name = "numero_matricula")
     private String numeroMatricula;
 }
